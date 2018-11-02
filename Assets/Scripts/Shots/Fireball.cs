7b4cad376696b4dcbc90535734388e3441a1baac
sNAttu <snattu91@gmail.com>
2018-11-03 00:21:08 +0200

    Shots applied to the project.

---
+++ Assets/Scripts/Shots/Fireball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shots
+{
+    public class Fireball : MonoBehaviour
+    {
+        public float Speed = 5.0f;
+
+        private SpriteRenderer _fireBallRenderer;
+
+        private void Start()
+        {
+            _fireBallRenderer = GetComponentInChildren<SpriteRenderer>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Translate(Vector2.up * Time.deltaTime * Speed);
+            if (!_fireBallRenderer.isVisible)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        
+
+    }
+}
